1591a93f65bbd221b966484fc9c63196f5ac7a9d
divyansh <divyanshojha2003@gmail.com>
2023-02-23 22:16:19 +0530

    Updates

---
+++ precache-manifest.1b690055254ac3f72e77937cdff97764.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = [
+  {
+    "revision": "1284a476e3d4b3530086",
+    "url": "/gamestore.github.io/static/css/main.2893e965.chunk.css"
+  },
+  {
+    "revision": "1284a476e3d4b3530086",
+    "url": "/gamestore.github.io/static/js/main.1284a476.chunk.js"
+  },
+  {
+    "revision": "2365806ba9c353f5a3a7",
+    "url": "/gamestore.github.io/static/js/1.2365806b.chunk.js"
+  },
+  {
+    "revision": "cdab1b8985d09ff6366c",
+    "url": "/gamestore.github.io/static/css/2.d0e6d85f.chunk.css"
+  },
+  {
+    "revision": "cdab1b8985d09ff6366c",
+    "url": "/gamestore.github.io/static/js/2.cdab1b89.chunk.js"
+  },
+  {
+    "revision": "ae588dd59f43d286d8a5",
+    "url": "/gamestore.github.io/static/js/runtime~main.ae588dd5.js"
+  },
+  {
+    "revision": "8ee87a5a1f4219b9a1fb984f554662e3",
+    "url": "/gamestore.github.io/static/media/GTWalsheimPro-Bold.8ee87a5a.ttf"
+  },
+  {
+    "revision": "550c7b057b4c5e92c648fb468dc9fb08",
+    "url": "/gamestore.github.io/static/media/GTWalsheimPro-Regular.550c7b05.ttf"
+  },
+  {
+    "revision": "f2f36c73edf543e7d7c6dd0a030e3af8",
+    "url": "/gamestore.github.io/static/media/GTWalsheimPro-Medium.f2f36c73.ttf"
+  },
+  {
+    "revision": "3745e321a3e602f37ac028aa3ffd443d",
+    "url": "/gamestore.github.io/static/media/GTWalsheimPro-Light.3745e321.ttf"
+  },
+  {
+    "revision": "3583eeee03e9dcad98ff675b7e4dbaf2",
+    "url": "/gamestore.github.io/static/media/GTWalsheimPro-UltraBold.3583eeee.ttf"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gamestore.github.io/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6feda395b9d43b0cd4afee6a8a203e25",
+    "url": "/gamestore.github.io/static/media/googleplaybadge.6feda395.png"
+  },
+  {
+    "revision": "f0257f8b73bd8e9dc7797ae39a1b2d17",
+    "url": "/gamestore.github.io/static/media/twitter.f0257f8b.png"
+  },
+  {
+    "revision": "8f22eac1c317ba5d3f3b9994e07f7168",
+    "url": "/gamestore.github.io/static/media/instagram.8f22eac1.png"
+  },
+  {
+    "revision": "1204896d5e44a3495fbdcc134fe82033",
+    "url": "/gamestore.github.io/static/media/404.1204896d.png"
+  },
+  {
+    "revision": "6dc0a35554cc79dbd02d3052f8abfd7b",
+    "url": "/gamestore.github.io/static/media/pyke.6dc0a355.mp4"
+  },
+  {
+    "revision": "141f8029d306c3f1354bdf3857b4712c",
+    "url": "/gamestore.github.io/static/media/logo.141f8029.svg"
+  },
+  {
+    "revision": "22a4a7bb54dd06fc88debfe73e63ee9a",
+    "url": "/gamestore.github.io/static/media/browse.22a4a7bb.svg"
+  },
+  {
+    "revision": "bb5cc033c3066e2ee48a0f99ed697408",
+    "url": "/gamestore.github.io/static/media/cart.bb5cc033.svg"
+  },
+  {
+    "revision": "f631f7eb2a715a764e22641e13f528fd",
+    "url": "/gamestore.github.io/static/media/github.f631f7eb.svg"
+  },
+  {
+    "revision": "0f55b056d1a95d74826dfe4895cc0d4c",
+    "url": "/gamestore.github.io/static/media/search.0f55b056.svg"
+  },
+  {
+    "revision": "488b169860d808067e1b82d3c101beda",
+    "url": "/gamestore.github.io/static/media/grid.488b1698.svg"
+  },
+  {
+    "revision": "4eced4e0ba5a2f69c216abfa3871b7a1",
+    "url": "/gamestore.github.io/static/media/columns.4eced4e0.svg"
+  },
+  {
+    "revision": "a062fada59c9f1343589331cdf4e455f",
+    "url": "/gamestore.github.io/static/media/wishlist.a062fada.svg"
+  },
+  {
+    "revision": "b651216eaca563717b90e980b371a7dd",
+    "url": "/gamestore.github.io/static/media/ratings.b651216e.svg"
+  },
+  {
+    "revision": "552e8fad9cc602e528ad255ec9f33774",
+    "url": "/gamestore.github.io/static/media/reviews.552e8fad.svg"
+  },
+  {
+    "revision": "9a8789f96838a2d94e73cee5c366ec02",
+    "url": "/gamestore.github.io/static/media/action.9a8789f9.svg"
+  },
+  {
+    "revision": "5de241bb3fd1c23e0909cec1bfd01745",
+    "url": "/gamestore.github.io/static/media/strategy.5de241bb.svg"
+  },
+  {
+    "revision": "5f3b474370a30edfb127aeb98e9ab94c",
+    "url": "/gamestore.github.io/static/media/RPG.5f3b4743.svg"
+  },
+  {
+    "revision": "7a1f5d3c3cae5a349489d6f08b19910e",
+    "url": "/gamestore.github.io/static/media/shooter.7a1f5d3c.svg"
+  },
+  {
+    "revision": "a8aeeb6549606daf0468ca1a4f709b1b",
+    "url": "/gamestore.github.io/static/media/adventure.a8aeeb65.svg"
+  },
+  {
+    "revision": "22b53c0d967c91272ec7e28dfbeea7de",
+    "url": "/gamestore.github.io/static/media/puzzle.22b53c0d.svg"
+  },
+  {
+    "revision": "b887382e1512f5b61805a757fda7ae63",
+    "url": "/gamestore.github.io/static/media/racing.b887382e.svg"
+  },
+  {
+    "revision": "0070c8312fd40f55f6bec3813a87c2f3",
+    "url": "/gamestore.github.io/static/media/sports.0070c831.svg"
+  },
+  {
+    "revision": "f4143dfa3251298c6192c44ca6295bd8",
+    "url": "/gamestore.github.io/static/media/like.f4143dfa.svg"
+  },
+  {
+    "revision": "ac975ec5356abba48015befc5c924958",
+    "url": "/gamestore.github.io/static/media/add.ac975ec5.svg"
+  },
+  {
+    "revision": "ce253b8957f993132cc769e3e9bcfac9",
+    "url": "/gamestore.github.io/static/media/added.ce253b89.svg"
+  },
+  {
+    "revision": "3b22bbc7b31f23a99531aee1d211a112",
+    "url": "/gamestore.github.io/static/media/arrowRight.3b22bbc7.svg"
+  },
+  {
+    "revision": "791217918579dc77f9ffefb07679c599",
+    "url": "/gamestore.github.io/static/media/cross.79121791.svg"
+  },
+  {
+    "revision": "2928664fe1fc6aca88583a6f606d60ba",
+    "url": "/gamestore.github.io/static/media/appstorebadge.2928664f.svg"
+  },
+  {
+    "revision": "370c8f92ddfdc11668a4e3ed3910fe4f",
+    "url": "/gamestore.github.io/static/media/arrow.370c8f92.svg"
+  },
+  {
+    "revision": "afbd21395505969ebf5a1dc30a0f3517",
+    "url": "/gamestore.github.io/static/media/up.afbd2139.svg"
+  },
+  {
+    "revision": "cee5462a9ee92b34ec9002643878cf8f",
+    "url": "/gamestore.github.io/static/media/down.cee5462a.svg"
+  },
+  {
+    "revision": "906d3d5d72030a8b6dd64b283b718b1d",
+    "url": "/gamestore.github.io/static/media/left.906d3d5d.svg"
+  },
+  {
+    "revision": "e1661ffa54614e9981b8431cf9948ff2",
+    "url": "/gamestore.github.io/static/media/right.e1661ffa.svg"
+  },
+  {
+    "revision": "9e99fe87ebdc5325e01b9eaf73b97ba5",
+    "url": "/gamestore.github.io/static/media/dot.9e99fe87.svg"
+  },
+  {
+    "revision": "a6d8c48cdc66316c579ced5a55cb88ac",
+    "url": "/gamestore.github.io/static/media/githublogo.a6d8c48c.svg"
+  },
+  {
+    "revision": "0c4b11b57af9b8a1bbb0377381976ac0",
+    "url": "/gamestore.github.io/static/media/enter.0c4b11b5.svg"
+  },
+  {
+    "revision": "bf76eee19a9ad5663ee45fd6dfc2015d",
+    "url": "/gamestore.github.io/static/media/dice.bf76eee1.svg"
+  },
+  {
+    "revision": "2d6fea87c6fabd7f073e6f117024426a",
+    "url": "/gamestore.github.io/static/media/linkedin.2d6fea87.svg"
+  },
+  {
+    "revision": "dae5b83a614572de84b6adc609218903",
+    "url": "/gamestore.github.io/static/media/game.dae5b83a.svg"
+  },
+  {
+    "revision": "04c50ad048601ca77f12e6b9158b8e3e",
+    "url": "/gamestore.github.io/static/media/notfound.04c50ad0.svg"
+  },
+  {
+    "revision": "9ce9551b4f5f2fb16acf8100ac6b49b6",
+    "url": "/gamestore.github.io/static/media/notfoundquery.9ce9551b.svg"
+  },
+  {
+    "revision": "be291e3cdc3077e4c5dee0ffe74d8c0f",
+    "url": "/gamestore.github.io/static/media/git.be291e3c.svg"
+  },
+  {
+    "revision": "a1d6dec65bc97fccad98eac428743c9a",
+    "url": "/gamestore.github.io/static/media/performance.a1d6dec6.svg"
+  },
+  {
+    "revision": "f93098b89b69f210deacaccf7bef961a",
+    "url": "/gamestore.github.io/static/media/sources.f93098b8.svg"
+  },
+  {
+    "revision": "75fd3c457eebc238487fc603ecd7190e",
+    "url": "/gamestore.github.io/index.html"
+  }
+];
